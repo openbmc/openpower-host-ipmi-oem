a49c63f6d9e60631f2af6f4ac546f96791cb2040
Matt Spinler <spinler@us.ibm.com>
2018-09-25 11:21:06 -0500

    Enable local users in the password reset OEM cmd
    Along with resetting the password, local users should
    also be enabled again so one can ssh into the system
    with a local user account again.
    
    This is accomplished by setting the UserEnabled property
    on every user object.
    
    Tested:  Saw users were enabled when running
             ipmitool raw 0x3a 0x11 from the host.
    
    Change-Id: I5a313cf82e2596cb797b8b9431631a3fe4ffca8d
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>

--- oemhandler.cpp
+++ oemhandler.cpp
@@ -1,6 +1,7 @@
 #include "oemhandler.hpp"
 #include "config.h"
 #include "elog-errors.hpp"
+#include "local_users.hpp"
 #include <host-ipmid/ipmid-api.h>
 #include <host-ipmid/ipmid-host-cmd.hpp>
 #include <fstream>
@@ -211,7 +212,11 @@ ipmi_ret_t ipmi_ibm_oem_reset_bmc_password(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
                                            ipmi_data_len_t data_len,
                                            ipmi_context_t context)
 {
-    return IPMI_CC_OK;
+    ipmi_ret_t rc;
+
+    rc = local::users::enableUsers();
+
+    return rc;
 }
 
 namespace {

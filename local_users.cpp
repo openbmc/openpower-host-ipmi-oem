82e5cc4a0f45a3bed2c990deaea143506d6ce5b4
Matt Spinler <spinler@us.ibm.com>
2018-10-02 15:56:09 -0500

    Reset root password in BMC auth reset cmd
    Resets the root password back to the default one
    used on OpenBMC systems.  It uses the pam_unix module
    to change the password in /etc/shadow.
    
    As a reminder, this IPMI command is not on the host
    IPMI whitelist, so if a user wants to prevent the host
    from running this command, the OEM command to turn on
    whitelisting should be used.
    
    Change-Id: Ie64fcc6ed564398802fe31690cebd52f8ae0eacc
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>

--- local_users.cpp
+++ local_users.cpp
@@ -2,6 +2,9 @@
 #include "local_users.hpp"
 #include <host-ipmid/ipmid-host-cmd.hpp>
 #include <phosphor-logging/log.hpp>
+#include <security/pam_misc.h>
+#include <security/pam_appl.h>
+#include <iostream>
 
 namespace local
 {
@@ -18,6 +21,15 @@ using DbusService = std::string;
 using DbusInterface = std::string;
 using ObjectTree = std::map<DbusObjectPath,
                             std::map<DbusService, std::vector<DbusInterface>>>;
+
+constexpr auto pamService = "passwd";
+
+pam_response* respData = NULL;
+
+struct Password
+{
+    const char* newPassword;
+};
 
 /**
  * @brief Gets a list of all local users in the form of GetSubTree
@@ -105,6 +117,87 @@ ipmi_ret_t enableUsers()
     }
 
     return IPMI_CC_OK;
+}
+
+/**
+ * PAM conversation function for changing the password.
+ * This is called by the PAM modules.
+ *
+ * @param[in] nMsg - number of messages in next argument
+ * @param[in] msg - array of pam_message pointers
+ * @param[out] resp - array of response objects
+ * @param[in] appPtr - application data pointer
+ *
+ * @return PAM_SUCCESS
+ */
+int changePassword(int nMsg, const pam_message** msg,
+                   pam_response** resp, void* appPtr)
+{
+    auto appData = static_cast<Password*>(appPtr);
+    respData = static_cast<pam_response*>(calloc(nMsg, sizeof(pam_response)));
+
+    memset(respData, 0, sizeof(pam_response) * nMsg);
+    *resp = respData;
+
+    for (int i = 0; i < nMsg; i++)
+    {
+        if (msg[0]->msg_style == PAM_PROMPT_ECHO_OFF)
+        {
+            respData[i].resp = strdup(appData->newPassword);
+        }
+    }
+    respData->resp_retcode = 0;
+
+    return PAM_SUCCESS;
+}
+
+ipmi_ret_t resetRootPassword()
+{
+    ipmi_ret_t ipmiRC = IPMI_CC_OK;
+    pam_conv conv;
+    pam_handle_t* pamh = nullptr;
+    Password appData;
+    int rc;
+
+    appData.newPassword = DEFAULT_PW;
+
+    conv.appdata_ptr = static_cast<void*>(&appData);
+    conv.conv = changePassword;
+
+    try
+    {
+        rc = pam_start(pamService, "root", &conv, &pamh);
+        if (rc != PAM_SUCCESS)
+        {
+            log<level::ERR>("pam_start failed",
+                    entry("RC=%d\n", rc));
+            throw std::exception();
+        }
+
+        rc = pam_chauthtok(pamh, 0);
+        if (rc != PAM_SUCCESS)
+        {
+            log<level::ERR>("pam_chauthtok failed",
+                    entry("RC=%d\n", rc));
+            throw std::exception();
+        }
+    }
+    catch (std::exception& e)
+    {
+        ipmiRC = IPMI_CC_UNSPECIFIED_ERROR;
+    }
+
+    if (pamh)
+    {
+        rc = pam_end(pamh, PAM_SUCCESS);
+        if (rc != PAM_SUCCESS)
+        {
+            log<level::INFO>("pam_end failed",
+                    entry("RC=%d", rc));
+        }
+    }
+
+    return ipmiRC;
 }
 
 }
